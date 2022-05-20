78ea79c6ec776c73614b6c21d551d0f213011874
LouisBerBiq <louis.beriotbiquet@gmail.com>
2022-05-21 01:34:36 +0200

    Renamed "products" post type to "product"

--- wp-content/themes/antilope/functions.php
+++ wp-content/themes/antilope/functions.php
@@ -7,7 +7,7 @@ require_once(__DIR__ . '/functionsUtilities.php');
 require_once(__DIR__ . '/functionsForm.php');
 
 // register a custom post type for products
-register_post_type( 'products', [
+register_post_type( 'product', [
 	'labels' => [
 		'name' => 'Produits',
 		'singular_name' => 'Produit'
--- wp-content/themes/antilope/template-product.php
+++ wp-content/themes/antilope/template-product.php
@@ -1,10 +1,11 @@
 <?php /* Template Name: Product page template */ ?>
 <?php get_header(); ?>
-<?php if(have_posts()): while(have_posts()): the_post(); ?>
+<?php if(($products = atl_get_products(1))->have_posts()): while($products->have_posts()): $products->the_post(); ?>
 	<main class="layout about">
 		<h2 class="about__title"><?= get_the_title(); ?></h2>
 		<div class="about__content">
 			<?php the_content(); ?>
+			<?php get_post_custom( int $post_id ) ?>
 		</div>
 	</main>
 <?php endwhile; endif; ?>

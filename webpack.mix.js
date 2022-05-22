9a0d5b8a67ae61d602c824d674a38824cfff11f5
LouisBerBiq <louis.beriotbiquet@gmail.com>
2022-05-23 01:09:59 +0200

    Changed webpack.mix.js to reflect the project name

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,14 +11,14 @@ const mix = require('laravel-mix');
  |
  */
 
-mix .setPublicPath('./wp-content/themes/portfolio/public')
-    .js('wp-content/themes/portfolio/resources/js/script.js', 'wp-content/themes/portfolio/public/js/')
-    .sass('wp-content/themes/portfolio/resources/sass/style.scss', 'wp-content/themes/portfolio/public/css/')
+mix .setPublicPath('./wp-content/themes/antilope/public')
+    .js('wp-content/themes/antilope/resources/js/script.js', 'wp-content/themes/antilope/public/js/')
+    .sass('wp-content/themes/antilope/resources/sass/style.scss', 'wp-content/themes/antilope/public/css/')
     .options({
         processCssUrls: false
     })
     .browserSync({
-        proxy: 'portfolio.test',
+        proxy: 'antilope.test',
         notify: false
     })
     .version();

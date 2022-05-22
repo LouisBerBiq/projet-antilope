c771f78e37faa91aa31646174b999663d15ace07
LouisBerBiq <louis.beriotbiquet@gmail.com>
2022-05-23 01:10:57 +0200

    Added proper dynamic head title

--- wp-content/themes/antilope/header.php
+++ wp-content/themes/antilope/header.php
@@ -8,7 +8,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
 		<title>
-			Antilope - 
+			<?= wp_title('-', true, 'right') . get_bloginfo('name') ?>
 		</title>
 		<link rel="stylesheet" type="text/css" href="./css/style.css">
 		<!--[if lt IE 9]>

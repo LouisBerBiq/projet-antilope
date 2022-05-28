b99573da29c0cf2d4a91edb079dc027367bca59d
LouisBerBiq <louis.beriotbiquet@gmail.com>
2022-05-28 16:57:25 +0200

    Restructured invex page for contant & styling

--- wp-content/themes/antilope/index.php
+++ wp-content/themes/antilope/index.php
@@ -1,23 +1,32 @@
 <?php get_header(); ?>
 
 <main class="layout">
-	<section class="layout__intro">
-		<h2 class="layout__title">
-			<?= str_replace( ':project', get_bloginfo('name'), __('Bienvenue sur le site du :project !', 'atl')); ?>
+	<section class="layout__intro intro">
+		<div class="intro__video-wrapper video-wrapper">
+			<video class="video-wrapper__video" poster="<?= atl_mix('/images/thumbnail1.webp') ?>" playsinline autoplay muted loop>
+				<source type="video/webm" src="">
+				<source type="video/mp4" src="">
+			</video>
+		</div>
+		<h2 class="intro__title">
+			<?= str_replace( ':project', get_bloginfo('name'), __('Bienvenue <br>sur le site du <br> :project !', 'atl')); ?>
 		</h2>
-		<p class="layout__tagline"><?= get_bloginfo('description') ?></p>
-		<section class="layout__products products">
-			<h2 class="products__title"><?= __('My latest works', 'atl'); ?></h2>
-			<a href="<?= get_post_type_archive_link('product'); ?>" class="products__all"><?= __('Voir tous nos modules', 'atl'); ?></a>
-			<div class="products__container">
-				<?php 
-				if(($products = atl_get_products(1))->have_posts()): while($products->have_posts()): $products->the_post();
-					include(__DIR__ . '/partials/product-card.php');
-				endwhile; else: ?>
-					<p class="products__empty"><?= __('Nous n\'avons pas de modules à afficher pour le moment', 'atl'); ?></p>
-				<?php endif; ?>
-			</div>
-		</section>
+		<p class="intro__tagline"><?= __('Nous faisons des appareils pour aider l\'environement. <br>Et rendre la vie meilleure', 'atl') ?></p>
+		<div class="intro__button-wrapper">
+			<a href="<?= get_post_type_archive_link('product'); ?>" class="intro__button"><?= __('Voir tous nos modules', 'atl'); ?></a>
+			<a href="<?= get_post_type_archive_link('question'); ?>" class="intro__button">?</a>
+		</div>
+	</section>
+	<section class="layout__products products">
+		<h2 class="products__title"><?= __('Nos derniers modules', 'atl'); ?></h2>
+		<div class="products__container">
+			<?php 
+			if(($products = atl_get_products(1))->have_posts()): while($products->have_posts()): $products->the_post();
+				include(__DIR__ . '/partials/product-card.php');
+			endwhile; else: ?>
+				<p class="products__empty"><?= __('Nous n\'avons pas de modules à afficher pour le moment', 'atl'); ?></p>
+			<?php endif; ?>
+		</div>
 	</section>
 </main>
 <div class="form__feedback">

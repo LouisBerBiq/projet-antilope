06df75be4c20aa8233078d3312819c174569b3a6
LouisBerBiq <louis.beriotbiquet@gmail.com>
2022-05-31 01:55:48 +0200

    Added 404 page (with questionable placeholder text)

---
+++ wp-content/themes/antilope/404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<main class="lost" id="main">
+	<p class="lost__paragraph">
+		<!-- // TODO: this is obviously a funny placeholder I typed in at 1 in the morning, make up a real 404 message oh and maybe some animation idk -->
+		<?= __('OOPSIE WOOPSIE!! Uwu We made a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!','atl'); ?>
+	</p>
+</main>
+
+<?php get_footer(); ?>

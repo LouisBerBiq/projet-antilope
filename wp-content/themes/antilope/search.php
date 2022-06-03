3bd093680c4ead36e09045277458e271e2ae47f4
LouisBerBiq <louis.beriotbiquet@gmail.com>
2022-06-03 19:02:23 +0200

    Grammar & broken emoji

--- wp-content/themes/antilope/search.php
+++ wp-content/themes/antilope/search.php
@@ -2,14 +2,13 @@
 
 <main class="layout">
 	<section class="results">
-		<h2 class="results__title"><?= __('Les question correspondant à votre recherche','atl'); ?></h2>
+		<h2 class="results__title"><?= __('Les questions correspondantes à votre recherche&nbsp;:','atl'); ?></h2>
 		<div class="results__container">
 		<?php 
 		if(($trips = atl_get_questions(10, get_search_query()))->have_posts()): while($trips->have_posts()): $trips->the_post();
 			include(__DIR__ . '/partials/question-collapsible.php');
 		endwhile; else: ?>
-			<!-- // TODO: make emoji -->
-			<p class="results__empty"><?= __('Votre recherche n\'a abouti à rien &#1F641;.','atl'); ?></p>
+			<p class="results__empty"><?= __('Votre recherche n\'a abouti à rien 🙁.','atl'); ?></p>
 		<?php endif; ?>
 		</div>
 	</section>

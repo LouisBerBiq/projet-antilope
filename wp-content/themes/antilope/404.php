f35f0607c8c306806888a9cce13585b74dc78b19
LouisBerBiq <louis.beriotbiquet@gmail.com>
2022-06-04 01:20:43 +0200

    Refined 404 page

--- wp-content/themes/antilope/404.php
+++ wp-content/themes/antilope/404.php
@@ -1,9 +1,20 @@
 <?php get_header(); ?>
 
 <main class="lost" id="main">
+	<h2 class="lost__title">
+		<?= __('Oops…','atl'); ?>
+	</h2>
 	<p class="lost__paragraph">
-		<!-- // TODO: this is obviously a funny placeholder I typed in at 1 in the morning, make up a real 404 message oh and maybe some animation idk -->
-		<?= __('OOPSIE WOOPSIE!! Uwu We made a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!','atl'); ?>
+		<?php
+			$messages = [
+				__('Il s\'emblerait que vous ayez pris un mauvais chemin…','atl'),
+				__('Il s\'emblerait que vous vous êtes perdu…', 'atl'),
+				__('heu… essayez de revenir en arrière.', 'atl'),
+			];
+		
+			echo $messages[array_rand($messages, 1)];
+		?>
+		<!-- // TODO: propose to send a message about it and maybe some animation idk -->
 	</p>
 </main>
 

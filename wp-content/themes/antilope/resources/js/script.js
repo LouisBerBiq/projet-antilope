d2966f12cb91b9e58db74a1ecca2e1bc6dca7087
LouisBerBiq <louis.beriotbiquet@gmail.com>
2022-06-03 02:11:24 +0200

    Added class name based fade-in on scroll (still needs to be applied on styles)

--- wp-content/themes/antilope/resources/js/script.js
+++ wp-content/themes/antilope/resources/js/script.js
@@ -9,7 +9,30 @@ class ATL_Controller
 	// after DOM
 	run()
 	{
+		// TODO: "for each add event...?"
+		// TODO: add on load event
+		document.querySelectorAll('[class*="fadeable--"]').forEach((el) => {
+			window.addEventListener("scroll", () => {
+				if (window.innerHeight + window.scrollY >= this.getElementPos(el).y || window.innerHeight + window.scrollY >= this.getElementPos(el).y) {
 
+					// is visible/above
+					el.classList.add('fadeIn');
+				}
+			});
+		});
+	}
+
+	// snatched from https://stackoverflow.com/a/35417781/17701651
+	getElementPos(el, rel)
+	{
+		var x=0, y=0;
+		do {
+			x += el.offsetLeft;
+			y += el.offsetTop;
+			el = el.offsetParent;
+		}
+		while (el != rel)
+		return {x:x, y:y};
 	}
 }
 

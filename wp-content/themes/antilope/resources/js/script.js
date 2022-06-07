2e1911a514e8481c734999b487c6dfd5141908db
LouisBerBiq <louis.beriotbiquet@gmail.com>
2022-06-07 03:15:05 +0200

    Added arrow fade when scrolling initial view (still buggy)

--- wp-content/themes/antilope/resources/js/script.js
+++ wp-content/themes/antilope/resources/js/script.js
@@ -10,9 +10,13 @@ class ATL_Controller
 	run()
 	{
 
-		document.querySelectorAll('[class*="fadeable--"]').forEach((el) => {
-			window.addEventListener("scroll", this.FadeInOnVisible(el), { once: true }); // not using an arrow func makes this work on load too
-		});
+		let discoveryArrow = document.querySelector('.intro__scroll-down');
+
+		document.querySelectorAll('[class*="fadeable--"]').forEach((el) => { // not using an arrow func makes this work on initial load
+			window.addEventListener("scroll", () => {
+				this.FadeInOnVisible(el);
+				this.FadeDiscoveryArrow(discoveryArrow);
+			}, { once: true });});
 	}
 
 	// snatched from https://stackoverflow.com/a/35417781/17701651
@@ -33,6 +37,15 @@ class ATL_Controller
 		if (window.innerHeight + window.scrollY >= this.getElementPos(el).y || window.innerHeight + window.scrollY >= this.getElementPos(el).y) {
 			// is visible/above
 			el.classList.add('fadeIn');
+		}
+	}
+	FadeDiscoveryArrow(discoveryArrow)
+	{
+		let position = discoveryArrow.offsetTop;
+		let scrolled = document.scrollingElement.scrollTop;
+
+		if(scrolled > position + 100){
+			discoveryArrow.classList.add('hidden');
 		}
 	}
 	// showPrivacyPolicy()

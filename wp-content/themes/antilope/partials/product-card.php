2094053bd0e111b5a1435f3e80181caeb0387a3a
LouisBerBiq <louis.beriotbiquet@gmail.com>
2022-06-10 20:22:43 +0200

    Auto module card color

--- wp-content/themes/antilope/partials/product-card.php
+++ wp-content/themes/antilope/partials/product-card.php
@@ -1,5 +1,11 @@
 <a href="<?= get_the_permalink(); ?>" class="product__wrapper fadeable--bottom">
-	<article class="product" module="<?= strtolower(get_the_title()); ?>">
+	<style>
+		/* attr() doesn't work with colors yet */
+		#<?= strtolower(get_the_title()) ?> {
+			background-color: <?= get_field('color') ?>;
+		}
+	</style>
+	<article class="product" id="<?= strtolower(get_the_title()) ?>">
 		<div class="product__card">
 			<figure class="product__fig">
 				<?= get_the_post_thumbnail(null, '', ['class' => 'fig__thumb']); ?>

76f872da545def8b011ebdae064c71ebe8923526
LouisBerBiq <louis.beriotbiquet@gmail.com>
2022-06-13 01:41:02 +0200

    Changed header home logo

--- wp-content/themes/antilope/header.php
+++ wp-content/themes/antilope/header.php
@@ -31,7 +31,7 @@
 			<nav class="header__nav nav">
 				<h2 class="nav__title"><?= __('Navigation principale','atl') ?></h2>
 
-				<a href="<?= get_home_url() ?>" class="nav__home" autofocus><img src="<?= atl_mix('/images/favicon.svg'); ?>" alt=""></a>
+				<a href="<?= get_home_url() ?>" class="nav__home" autofocus><img src="<?= atl_mix('/images/home.svg'); ?>" alt=""></a>
 				<ul class="nav__container">
 					<?php foreach(atl_get_menu_items('header') as $link): ?>
 					<li class="<?= $link->getBemClasses('nav__item'); ?>">

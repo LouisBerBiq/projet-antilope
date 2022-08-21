a07bc1500856f0ca7c12d184eb20082ebd807a37
LouisBerBiq <louis.beriotbiquet@gmail.com>
2022-08-21 15:25:01 +0200

    product page now swaps the accent color for the product's color

--- wp-content/themes/antilope/single-product.php
+++ wp-content/themes/antilope/single-product.php
@@ -1,4 +1,12 @@
 <?php get_header(); ?>
+
+<style>
+	/* attr() doesn't work with colors yet */
+	:root {
+		--accent: <?= get_field('color') ?>;
+		--accent-lighter: #FF9838;
+	}
+</style>
 	<?php if(have_posts()): while(have_posts()): the_post(); ?>
 	<main class="layout singleProduct" id="main">
 		<aside class="singleProduct__images">
